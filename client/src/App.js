f526e31a2d5f486b98ef31fbf1cc5e3b2397edb2
keith <kdang@quartermaster.house>
2020-07-19 14:55:07 -0400

    Student as separate component

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,14 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import { fetchStudent } from "./actions/student";
-import "./App.css";
-import student from "./reducers/student";
+import Student from "./components/Student";
 
 class App extends Component {
-  componentDidMount() {
-    this.props.fetchStudent();
-  }
-
-  showStudents = () => {
-    const { student } = this.props;
+  render() {
     return (
       <div>
-        <h1>Student</h1>
-        <ul className="students">
-          <li key={student.id}>
-            {student.id}:{student.firstName}
-          </li>
-        </ul>
-      </div>
-    );
-  };
-  render() {
-    const { student } = this.props;
-    console.log("render", student);
-    return (
-      <div className="App">
-        {student.id !== "" ? (
-          this.showStudents()
-        ) : (
-          <div>
-            <h1>No Students :(</h1>
-            <button className="more" onClick={this.props.fetchStudent}>
-              Try Again?
-            </button>
-          </div>
-        )}
+        <Student />
       </div>
     );
   }
 }
 
-export default connect(({ student }) => ({ student }), { fetchStudent })(App);
+export default App;
---
+++ client/src/components/Student.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchStudent } from "../actions/student";
+import "../App.css";
+
+class Student extends Component {
+  componentDidMount() {
+    this.props.fetchStudent();
+  }
+
+  showStudents = () => {
+    const { student } = this.props;
+    return (
+      <div>
+        <h1>Student</h1>
+        <ul className="students">
+          <li key={student.id}>
+            {student.id}:{student.firstName}
+          </li>
+        </ul>
+      </div>
+    );
+  };
+  render() {
+    const { student } = this.props;
+    return (
+      <div className="App">
+        {student.id !== "" ? (
+          this.showStudents()
+        ) : (
+          <div>
+            <h1>No Students :(</h1>
+            <button className="more" onClick={this.props.fetchStudent}>
+              Try Again?
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default connect(({ student }) => ({ student }), { fetchStudent })(
+  Student
+);
